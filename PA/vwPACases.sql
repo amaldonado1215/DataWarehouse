1e2db63dc7511b54d68ceb2401d998b6c58339af
kimtanderson <kim.anderson@mpowerpractice.net>
2018-01-22 15:42:11 -0600

    #30

--- PA/vwPACases.sql
+++ PA/vwPACases.sql
@@ -27,6 +27,7 @@ SELECT        CR.PID,
 				Case
 					WHEN CR.PID = 682891 THEN 'NPPA Services' -- Ticket 2319 lauren
 					when CR.DOS <'2017-10-01' and CR.surgeon = 'Edward Seade, M.D.' and cr.[1st Insurance Category] = 'Letter of Protection'  then 'NPPA Services' --ticket 2386 lauren
+					WHEN sl.ContractType = 'Hybrid' and cr.[1st Insurance Category] = 'Blue Cross Blue Shield'  and RL.Status = 'SurgeonPA' then rtrim(SL.Entity)  --#30 kta
 				--ticket 2246 lauren: If the surgeon = Adam Bruggeman, M.D. and the Insurance Type = Letter of Protection,
 				--and the Tech does not equal "Kim Stewart, PA-C" then the correct entity is NPPA Services
 				    when cr.surgeon = 'Adam Bruggeman, M.D.' and cr.[1st Insurance Category] = 'Letter of Protection' and cr.tech <> 'Kim Stewart, PA-C' then 'NPPA Services'
@@ -37,7 +38,7 @@ SELECT        CR.PID,
 				--and the Tech is Kim Stewart, and the 1st ins type = BCBS then the case defers to SL  else NPPA (current default)
 					--when cr.dos > '2016-12-31' and cr.tech in ('Steve Greer, LSA','Jerold Greer') then 'Precision Assist of Dallas'--ticket 2373 lauren -- remove ticket 18 kta
 					when cr.dos > '2016-12-31' and (cr.tech like '%CSFA%' or cr.tech like '%LSA%') then 'Precision Assist of Dallas'--ticket 18 kta
-					when CR.DOS >= '2017-01-01' and CR.Surgeon = 'Adam Bruggeman, M.D.' and cr.tech in ('Kim Stewart, PA-C','Kimberly Stewart, MPAS, PA-C') and CR.[1st Insurance Category] = 'Blue Cross Blue Shield' then rtrim(SL.Entity) --ticket 2402 
+				-- #30 kta	when CR.DOS >= '2017-01-01' and CR.Surgeon = 'Adam Bruggeman, M.D.' and cr.tech in ('Kim Stewart, PA-C','Kimberly Stewart, MPAS, PA-C') and CR.[1st Insurance Category] = 'Blue Cross Blue Shield' then rtrim(SL.Entity) --ticket 2402 
 					when cr.dos < '2017-01-01' and CR.Surgeon = 'Adam Bruggeman, M.D.' and CR.[1st Insurance Category] = 'Blue Cross Blue Shield' then SL.Entity --ticket 2491 lauren
 					when CR.[1st Insurance Category] in ('Lein Case','Letter of Protection','Private Insurance') then rtrim(SL.Entity)--ticket 2354 un-do 2320
 					--when CR.[1st Insurance Category] in ('Lein Case','Private Insurance') then rtrim(SL.Entity)
