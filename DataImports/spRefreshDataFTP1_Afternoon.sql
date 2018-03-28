aca956b6c30d0c5612e4b2627e02895e71ce24ea
mPowerAdmin <35049556+mPowerAdmin@users.noreply.github.com>
2018-03-28 07:40:21 -0500

    Changes to nightly data loads from USMON

--- DataImports/spRefreshDataFTP1_Afternoon.sql
+++ DataImports/spRefreshDataFTP1_Afternoon.sql
@@ -17,6 +17,12 @@ ALTER  procedure [dbo].[spRefreshDataFTP1_Afternoon] as
 		drop table Insurance_Deposits_3350;
 	select * INTO [USMON_Local].dbo.Insurance_Deposits_3350 from [dbconnect.usmon.com].[USMON].dbo.Insurance_Deposits_3350
 
+	if exists (select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'invoice_history_3300') and (select count(*) from [dbconnect.usmon.com].[USMON].dbo.invoice_history_3300) > 0
+		drop table invoice_history_3300;
+	select * INTO [USMON_Local].dbo.invoice_history_3300 from [dbconnect.usmon.com].[USMON].dbo.invoice_history_3300
+
+
+
 	
 --
 	CREATE NONCLUSTERED INDEX IDX_CaseReport3300
